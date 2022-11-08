88ef00bfe6e8f080b2e592e27bb19b9a8a959a2b
mawshin <mawshin@hotmail.sg>
2022-11-08 16:11:31 +0800

    update change hash via link click

--- bootstrap/deep-link/js/scripts.js
+++ bootstrap/deep-link/js/scripts.js
@@ -2,7 +2,7 @@ $(function() {
 
 function pageLoad() {
     var hash = $(location).attr('hash');
-    var hash2 = window.location.href.split('#')[0];
+    //var hash2 = window.location.href.split('#')[0];
 
     hash = hash.substring(1, hash.length);
 
@@ -14,7 +14,9 @@ function pageLoad() {
 	$('#' + hash).show();
     } else {
 	$('.sections').hide();
-	location.hash = 'blah';
+	// line below will add the the hashtag to the back of the URL
+	// location.hash = 'blah';
+	
 	return false;
     }
 
@@ -28,4 +30,9 @@ $(window).on('hashchange', function(e){
     // Your Code goes here
     pageLoad();
 });
+
+$('.nav-link').click(function() {
+    location.hash = $(this).attr('id');
+});
+
 });
