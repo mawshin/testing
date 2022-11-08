676ed605a9c9823f3942487640cc81fc4e231e7f
mawshin <mawshin@hotmail.sg>
2022-11-08 10:10:33 +0800

    update hashtag retrieval of name

--- bootstrap/deep-link/js/scripts.js
+++ bootstrap/deep-link/js/scripts.js
@@ -1,7 +1,9 @@
 $(function() {
 console.log('script loaded');
     var hash = $(location).attr('hash');
-    var hash2 = window.location.href.split('#')[0]
+    var hash2 = window.location.href.split('#')[0];
+
+    hash.substring(1, hash.length);
 
     console.log(hash);
     console.log(hash2);
