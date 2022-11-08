43ba39e78c6a632bb0eb69036881c81e23155342
mawshin <mawshin@hotmail.sg>
2022-11-08 10:14:01 +0800

    update hash script

--- bootstrap/deep-link/js/scripts.js
+++ bootstrap/deep-link/js/scripts.js
@@ -3,7 +3,7 @@ console.log('script loaded');
     var hash = $(location).attr('hash');
     var hash2 = window.location.href.split('#')[0];
 
-    hash.substring(1, hash.length);
+    hash = hash.substring(1, hash.length);
 
     console.log(hash);
     console.log(hash2);
